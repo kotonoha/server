fa7414f76a1e7fb3e82dee6d6786cd9ce526aaa4
Dijkstra <me@dijkstra.ru>
2012-07-18 13:40:31 +0600

    Replaced map with Map in DictAbbreviations

--- src/main/scala/org/eiennohito/kotonoha/dict/DictAbbreviations.scala
+++ src/main/scala/org/eiennohito/kotonoha/dict/DictAbbreviations.scala
@@ -1,19 +1,18 @@
 package org.eiennohito.kotonoha.dict
 
 object DictAbbreviations {
-  def Abbr2PartOfSpeechName(abbr:Option[String]):Option[String] =
-    abbr map {
-      case "n" => "名詞"
-      case "v1" => "一段動詞"
-      case "v5u" => "五段動詞・ア行"
-      case "v5k" => "五段動詞・カ行"
-      case "v5g" => "五段動詞・ガ行"
-      case "v5s" => "五段動詞・シ行"
-      case "v5t" => "五段動詞・ツ行"
-      case "v5n" => "五段動詞・ナ行"
-      case "v5b" => "五段動詞・バ行"
-      case "v5m" => "五段動詞・マ行"
-      case "v5r" => "五段動詞・ラ行"
-      case "vk" => "動詞 （来る)"
-    }
+  val Abbr2PartOfSpeechName = Map (
+      "n" -> "名詞",
+      "v1" -> "一段動詞",
+      "v5u" -> "五段動詞・ア行",
+      "v5k" -> "五段動詞・カ行",
+      "v5g" -> "五段動詞・ガ行",
+      "v5s" -> "五段動詞・シ行",
+      "v5t" -> "五段動詞・ツ行",
+      "v5n" -> "五段動詞・ナ行",
+      "v5b" -> "五段動詞・バ行",
+      "v5m" -> "五段動詞・マ行",
+      "v5r" -> "五段動詞・ラ行",
+      "vk" -> "動詞 （来る)"
+    )
 }
