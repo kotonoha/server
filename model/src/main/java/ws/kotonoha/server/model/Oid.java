8333c34607392470908978eadfad7309c86c56d4
Arseny Tolmachev <eiennohito@gmail.com>
2015-01-20 15:36:04 +0900

    fixed Oid.java for compiling on java 1.8

--- model/src/main/java/ws/kotonoha/server/model/Oid.java
+++ model/src/main/java/ws/kotonoha/server/model/Oid.java
@@ -30,15 +30,9 @@ import java.util.concurrent.atomic.AtomicInteger;
  * A globally unique identifier for objects.
  * <p>Consists of 12 bytes, divided as follows:
  * <blockquote><pre>
- * <table border="1">
- * <tr><td>0</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td>
- *     <td>7</td><td>8</td><td>9</td><td>10</td><td>11</td></tr>
- * <tr><td colspan="4">time</td><td colspan="3">machine</td>
- *     <td colspan="2">pid</td><td colspan="3">inc</td></tr>
- * </table>
+ *     timestamp-4 machine-2 netrowk-2 counter-4
  * </pre></blockquote>
  *
- * @dochub objectids
  */
 public class Oid implements Comparable<Oid>, java.io.Serializable {
 
@@ -55,6 +49,7 @@ public class Oid implements Comparable<Oid>, java.io.Serializable {
 
   /**
    * Checks if a string could be an <code>ObjectId</code>.
+   * @param s input string
    *
    * @return whether the string could be an object id
    */
