fa5d181560b105aadebc85cc6ff14c70b6ed7912
Arseny Tolmachev <eiennohito@gmail.com>
2016-12-06 22:01:12 +0900

    remove useless test

--- src/test/scala/ws/kotonoha/server/util/SecurtiyUtilTest.scala
+++ src/test/scala/ws/kotonoha/server/util/SecurtiyUtilTest.scala
@@ -16,41 +16,12 @@
 
 package ws.kotonoha.server.util
 
-import org.scalatest.{FreeSpec, Matchers}
-import org.scalatest.matchers.ShouldMatchers
-import javax.crypto.Cipher
-
-import org.apache.commons.codec.binary.{Base64, Hex}
-import javax.crypto.spec.{IvParameterSpec, SecretKeySpec}
-
-import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher
-import org.bouncycastle.crypto.modes.CFBBlockCipher
-import org.bouncycastle.crypto.engines.AESFastEngine
-import org.bouncycastle.crypto.params.KeyParameter
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FreeSpec, Matchers}
 
 
 class SecurtiyUtilTest extends FreeSpec with Matchers with GeneratorDrivenPropertyChecks {
   "SecurityUtil" - {
-    "encodes url like in java with aes" in {
-      val key = "82e52a2bc0d78d206388788e897f7383"
-      val msg = "My greatest message!"
-
-      val binkey = Hex.decodeHex(key.toCharArray)
-      val ks = new SecretKeySpec(binkey, "AES")
-      val code = SecurityUtil.uriAesEncrypt(msg, binkey)
-      val bytes = new Base64(true).decode(code)
-      val aes = new PaddedBufferedBlockCipher(new CFBBlockCipher(new AESFastEngine(), 8))
-      val kp = new KeyParameter(binkey)
-      aes.init(false, kp)
-      val decr = new Array[Byte](aes.getOutputSize(bytes.length))
-      val len = aes.processBytes(bytes, 0, bytes.length, decr, 0)
-      val l1 = aes.doFinal(decr, len)
-      val resStr = new String(decr, 0, len + l1, "utf-8")
-      resStr should equal (msg)
-    }
-
-
     "encodes and decodes text with AES" in {
       forAll { plaintext: String =>
         val key = SecurityUtil.randomBytes(16)
